052df2c2ed867523e609e6bc77d59862f6818984
luoriwusheng <1197802519@qq.com>
2018-12-03 07:39:22 +0800

    picker-view

--- pages/query/query.js
+++ pages/query/query.js
@@ -1,10 +1,11 @@
 // pages/query/query.js
 Page({
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    imgs: [
+      "../images/tab/1.png",
+      "../images/tab/2.png",
+      "../images/tab/3.png"
+    ]
   },
 
   /**
@@ -23,53 +24,8 @@ Page({
       res[1].scrollTop // 显示区域的竖直滚动位置
     })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-   
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  change(e) {
+    console.log(e)
   }
+
 })
